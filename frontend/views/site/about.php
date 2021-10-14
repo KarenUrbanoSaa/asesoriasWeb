5c6bae8985b0f89c96f86f0858efee4c9224a6fc
KarenUrbano <quemojojojo@hotmail.com>
2021-10-14 10:42:15 -0500

    segundo commit para los archivos que no había subido. algunos no subieron

---
+++ frontend/views/site/about.php
@@ -0,0 +1,35 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = '¿Quienes somos?';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nisi numquam quidem alias at voluptates commodi tenetur. Hic tenetur minima ab labore molestias quas? Error repudiandae perferendis harum quas maxime?
+    Accusantium incidunt voluptatum obcaecati blanditiis tempora et vitae sunt tempore, veniam aliquam eum, pariatur illum dolor qui alias accusamus adipisci aut culpa dolorem quia reiciendis odio distinctio! Repudiandae, est corporis!
+    Dolorum sunt necessitatibus eligendi accusantium fugit perspiciatis incidunt inventore facilis quis et cupiditate voluptate saepe enim, ea, exercitationem voluptatum ullam magni harum ab cum provident, voluptates laboriosam distinctio! Minus, totam!
+    Magnam qui delectus voluptates doloribus! Vitae eaque repellat est dolorem possimus praesentium doloribus nobis quod totam incidunt commodi error fugiat, impedit eveniet, culpa odio aut. A quo quaerat autem voluptate?
+    Eius molestias cumque commodi est esse, deserunt modi quam tempora enim provident vero voluptate consequuntur labore iusto! Dolorem ex odit facere sint! Deleniti excepturi culpa impedit sapiente repellat accusamus at.
+    Voluptatibus in blanditiis vitae, id soluta nobis, minus repellat odit, similique placeat eos exercitationem? Culpa laudantium sunt obcaecati eligendi cupiditate libero at nostrum illum voluptates, nesciunt commodi saepe deleniti expedita.
+    Atque eveniet voluptas et placeat magni expedita ex sunt sequi, id dicta autem. Fugit magnam sit vitae accusamus delectus reprehenderit repellat cum sapiente. Tempora rerum, unde perferendis soluta enim ex?
+    Atque, reiciendis. Dicta laudantium veniam vitae, nisi odio officia? Qui temporibus harum corrupti non laudantium est minus aut omnis dolores rem sed sint perspiciatis cumque ex aliquam tempora, dolorem odit.
+    Expedita vel obcaecati et assumenda fugiat perspiciatis illum eius ea quasi saepe labore quaerat quia tempore, iste commodi impedit quas quo sit vitae ullam totam animi sed eum veniam! Eligendi.
+    Repellat quaerat ipsa doloremque iusto omnis nihil, illo cupiditate est accusamus, tempore quos perspiciatis ducimus alias. Quasi velit porro doloribus ex beatae delectus illum magni. Aut exercitationem facere omnis delectus.
+    Aliquam fuga doloribus, culpa placeat, aperiam adipisci quos ducimus minima ipsam sequi vel vitae eligendi ratione, et soluta totam inventore neque rem omnis quibusdam sit nulla quae! Fuga, consequatur praesentium?
+    Ullam perferendis culpa animi corrupti voluptatum quod et, nihil aliquid rem! Atque aperiam quod neque voluptas itaque harum libero nam! Laudantium culpa perferendis, fugiat sunt recusandae laborum deserunt voluptatibus voluptates.
+    Tempora soluta quidem odio impedit sint enim expedita dolores eaque, neque earum blanditiis nostrum quaerat laudantium doloribus, exercitationem repellendus ipsam tempore dolorum delectus. Amet dolor eos omnis in repellendus cupiditate!
+    Ratione dolorum recusandae, vero eius exercitationem, est, numquam maxime dolore id nisi quibusdam accusamus asperiores accusantium delectus deleniti? Labore sit iure voluptatibus provident aperiam aut doloribus cupiditate assumenda sapiente. Numquam!
+    Quaerat velit nam, sint deleniti, tempora consequatur incidunt consectetur at, molestiae eius minima eaque sunt qui aperiam quos perspiciatis. Ut ratione laborum autem unde reiciendis ipsa quas odio. Sint, perspiciatis.
+    Eos numquam sint suscipit magnam ipsum, ad impedit labore. Illo deserunt architecto soluta quidem itaque recusandae quo sequi excepturi, mollitia et praesentium impedit debitis consequuntur ullam nihil. Deleniti, maiores provident?
+    Veniam explicabo tempora nihil ipsa voluptatum eius, molestias ad, id libero quam, nulla eum aut placeat repellendus quibusdam expedita repudiandae distinctio. Unde, maxime illum minus quae placeat labore similique officiis?
+    Est eaque debitis in dolores iure nam quidem libero? Perferendis molestiae eaque aut ratione illo nemo ut reprehenderit, voluptatem autem beatae iure praesentium? Culpa magni repellat unde veritatis at excepturi.
+    Eius quia perferendis, nesciunt ducimus dolorum ad aut, fuga saepe, amet itaque tenetur. Cupiditate veniam explicabo recusandae ut quam impedit suscipit animi quaerat ullam facere? Molestias molestiae corporis minus minima.
+    Totam exercitationem amet iste voluptas ab optio vero dicta delectus qui accusantium, eveniet saepe aliquam omnis modi voluptate ipsum repellat placeat possimus quisquam illum tempora soluta nihil deserunt. Iure, voluptas!</p>
+
+    
+</div>
